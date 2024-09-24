689de4d6c2b98936e806f7e861ee7e873065d78d
rimaakter1 <rimac201262@gmail.com>
2024-09-24 12:23:47 +0600

    added donation amount event handler and donation history event handler

---
+++ js/index.js
@@ -0,0 +1,56 @@
+// history and donation button toggle
+
+const donationHistoryButton = document.getElementById('donate-history-btn');
+const donationButton = document.getElementById('donation-btn');
+
+donationHistoryButton.addEventListener('click', function () {
+    handleToggleButton(donationHistoryButton, donationButton, 'donation-history-list', 'donation-cards-container')
+})
+
+donationButton.addEventListener('click', function () {
+    handleToggleButton(donationButton, donationHistoryButton, 'donation-cards-container', 'donation-history-list')
+})
+
+// donation amount 
+
+const AllDonateAmountButton = document.querySelectorAll('.donate-amount-btn');
+// console.log(donateAmountButton);
+
+for (const donateAmountButton of AllDonateAmountButton) {
+    donateAmountButton.addEventListener('click', function (e) {
+        const donateAmount = e.target.parentNode.children[0];
+        const donateAmountNumber = parseFloat(donateAmount.value);
+
+        if (donateAmountNumber < 0 || isNaN(donateAmountNumber) || donateAmountNumber === "") {
+            alert('Invalid donation amount');
+            return;
+        }
+
+        const availableBalance = document.getElementById('available-balance');
+        const availableBalanceAmount = parseFloat(availableBalance.innerText);
+
+        if (availableBalanceAmount < donateAmountNumber) {
+            alert('Insufficient amount in your account');
+            return;
+        }
+
+        const currentBalance = availableBalanceAmount - donateAmountNumber;
+        availableBalance.innerText = currentBalance;
+        const totalDonateAmount = e.target.parentNode.parentNode.children[0].children[1].children[0];
+        const totalDonateAmountNumber = parseFloat(totalDonateAmount.innerText)
+        totalDonateAmount.innerText = totalDonateAmountNumber + donateAmountNumber;
+
+
+        // donation history
+        const locationTitle = e.target.parentNode.parentNode.children[1].innerText;
+        handleDonationHistory('donation-history-list', donateAmountNumber, locationTitle)
+
+
+
+    })
+}
+
+
+
+
+
---
+++ js/utilities.js
@@ -0,0 +1,28 @@
+
+// toggle button function 
+function handleToggleButton(historyBtn, donationBtn, hisotryId, cardsId) {
+    historyBtn.classList.add('bg-primary', 'text-secondary', 'font-semibold', 'border-0');
+    historyBtn.classList.remove('text-accent', 'font-medium', 'border', 'border-gray-400');
+
+    donationBtn.classList.add('text-accent', 'font-medium', 'border', 'border-gray-400');
+    donationBtn.classList.remove('bg-primary', 'text-secondary', 'font-semibold', 'border-0');
+
+    document.getElementById(hisotryId).classList.remove('hidden');
+    document.getElementById(cardsId).classList.add('hidden');
+}
+
+// donation history list function
+
+function handleDonationHistory(historyListId, donatationAmount, title) {
+    const donationHistoryList = document.getElementById(historyListId);
+    const HistoryDiv = document.createElement('div');
+    HistoryDiv.classList.add('border', 'border-gray-300', 'p-8', 'rounded-2xl', 'mb-6')
+
+    HistoryDiv.innerHTML = `
+            <h2 class="text-xl font-bold  mb-4 text-secondary">${donatationAmount} Taka ${title}
+            </h2>
+            <p>Date: ${new Date()}</p>
+    
+    `
+    donationHistoryList.appendChild(HistoryDiv);
+}
